5e489486d9db337706a2823d3e00c86b3f84afac
Tiago Ceccon <ceccon.t@hotmail.com>
2017-08-07 19:36:50 -0300

    Sketches datafile functionality for words

--- sandbox/hgsketch.c
+++ sandbox/hgsketch.c
@@ -1,30 +1,66 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<time.h>
 
 #define MAX_STRING 20
 #define MAX_HINT 75
 #define TOTAL_TRIES 5
 #define QUIT_SIGNAL '0'
 
-/*
-TODO: implement verification of winning the game
-V
+#define DATAFILENAME "words.dat"
+#define NUM_WORDS_DEFAULT_FILE 5
 
-TODO: display tries_left
+
+/*
+=======================
+// Gameplay
+TODO: implement verification of winning the game
 V
 
 TODO: implement a manual quit mechanism
 V
 
+TODO: decrement tries_left only when no matches were found for the guess
+V
+=======================
+
+=======================
+// Validation
 TODO: force only one character choice each time
 
+TODO: force only unused characters
+
+TODO: display used characters
+
+=======================
+
+=======================
+// Data structure
 TODO: Word structure
 V
 
 TODO: file storage / bin data
+V
 
 TODO: random access to bin data
+V
+- TODO: find number of entries on the file (to determine a random position)
+V
+=======================
+
+=======================
+// Interface
+TODO: display tries_left
+V
+
+TODO: check clear screen
+V
+
+TODO: define the visual presentation of the game
+
+TODO: implement the 'drawings'
+=======================
 */
 
 typedef struct word {
@@ -37,7 +73,7 @@ typedef struct word {
 
 WORD choose_word();
 void display_match(WORD chosen_word, int *mask, int tries_left);
-void reset_mask(int *mask);
+void reset_mask(int *mask, int val);
 int check_win(int *mask, int i_last_char);
 int end_of_string(char *string); // TODO: remove eventually
 
@@ -45,18 +81,20 @@ int main () {
     //char string[MAX_STRING] = "banana";
     int mask[MAX_STRING] = {0};
     char guess;
-    int i, i_last_char, tries_left, won = 0, play = 1;
+    int i, i_last_char, tries_left, won = 0, play = 1, found;
     WORD chosen_word;
+    
+    srand(time(0));
     
     //printf("%d\n", end_of_string(string));
     //exit(1);
     
-    chosen_word = choose_word();
     
     while (play) {
     	won = 0;
     	tries_left = TOTAL_TRIES;
-    	reset_mask(mask);
+    	reset_mask(mask, 0);
+    	chosen_word = choose_word();
     	i_last_char = strlen(chosen_word.string) - 1;
     	
     	while(tries_left > 0 && !won) {
@@ -72,17 +110,23 @@ int main () {
 				tries_left = 0;
 			} else {
 		
+				found = 0;
 				for (i=0; i<strlen(chosen_word.string); i++) {
 					if (chosen_word.string[i] == guess) {
 						mask[i] = 1;
 						won = check_win(mask, i_last_char);
+						found = 1;
 					}
 				}
-				tries_left--;
+				if (!found) {
+					tries_left--;
+				}
 			
 			}
 			
     	}
+    	
+    	reset_mask(mask, 1);
     	
     	display_match(chosen_word, mask, tries_left);
     	
@@ -98,7 +142,7 @@ int main () {
     }
     
     
-    printf("\nFIM\n");
+    printf("\nEND\n");
     return 0;
     
 }
@@ -106,11 +150,30 @@ int main () {
 
 WORD choose_word() {
 	WORD chosen;
+	FILE * datafile;
+	int entries_on_file, chosen_pos, filesize; // [0,NUM_OF_ENTRIES_ON_FILE]
 	
-	strcpy(chosen.string, "banana");
-	strcpy(chosen.hint1, "It's a fruit");
-	strcpy(chosen.hint2, "Associated with the tropics");
-	strcpy(chosen.hint3, "It's yellow");
+	//strcpy(chosen.string, "banana");
+	//strcpy(chosen.hint1, "It's a fruit");
+	//strcpy(chosen.hint2, "Associated with the tropics");
+	//strcpy(chosen.hint3, "It's yellow");
+	
+	datafile = fopen(DATAFILENAME, "rb");
+	
+	fseek(datafile, 0L, SEEK_END);
+	filesize = ftell(datafile);
+	
+	//fread(&chosen, sizeof(WORD), 1, datafile);
+	//entries_on_file = 4;
+	entries_on_file = filesize / sizeof(WORD);
+	//printf("======%d======", entries_on_file);
+	//chosen_pos = 3;
+	chosen_pos = (int) (rand() / (double) RAND_MAX * entries_on_file);
+	fseek(datafile, chosen_pos * sizeof(WORD), SEEK_SET); 
+	fread(&chosen, sizeof(WORD), 1, datafile);
+	
+	fclose(datafile);
+	
 	
 	return chosen;
 }
@@ -118,7 +181,9 @@ WORD choose_word() {
 void display_match(WORD chosen_word, int *mask, int tries_left) { 
 	int i;
 	
-	system("clear"); // TODO: find better solution than calling the system
+	//system("clear||cls"); 
+	// cls: Windows command to clear prompt; printf "\033c": same for bash
+	system("cls||printf \"\\033c\""); // TODO: find better solution than calling the system
 	printf("\n---------\n");
 	for (i=0; i<strlen(chosen_word.string); i++) {
 		if (mask[i]) {
@@ -134,11 +199,11 @@ void display_match(WORD chosen_word, int *mask, int tries_left) {
 	
 }
 
-void reset_mask(int *mask) {
+void reset_mask(int *mask, int val) {
 	int i;
 	
 	for (i=0; i < MAX_STRING; i++) {
-		mask[i] = 0;
+		mask[i] = val;
 	}
 	
 }
--- sandbox/hgutils.c
+++ sandbox/hgutils.c
@@ -7,13 +7,18 @@
 #define TOTAL_TRIES 5
 #define QUIT_SIGNAL '0'
 
+#define DATAFILENAME "words.dat"
+#define NUM_WORDS_DEFAULT_FILE 5
+
 /*
 TODO: Word structure
 V
 
 TODO: display_word
+V
 
 TODO: create bin file for Words
+V
 
 TODO: random access to bin data
 */
@@ -27,14 +32,22 @@ typedef struct word {
 
 
 void display_word(WORD w);
+int create_default_datafile(char * filename);
+int inspect_file(char * filename);
 
 int main() {
+	/*
 	WORD w = {"banana",
 			  "It's a fruit",
 			  "Associated with the tropics",
 			  "It's yellow"};
 			  
 	display_word(w);
+	*/
+	
+	//create_default_datafile("new_default_file.dat");
+	
+	inspect_file(DATAFILENAME);
 	
 	return 0;
 }
@@ -49,3 +62,85 @@ void display_word(WORD w) {
 	printf("\n===== END DISPLAY WORD ===== \n");
 	
 }
+
+int create_default_datafile(char * filename) {
+	FILE * datafile;
+	int i;
+	WORD words[] = {
+					{ 
+					  "banana",
+					  "It's a fruit",
+					  "Associated with the tropics",
+					  "It's yellow"
+					},
+					{ 
+					  "hangman",
+					  "A game",
+					  "Usually played with pen and paper",
+					  "Focused on words"
+					},
+					{ 
+					  "argentina",
+					  "A country",
+					  "On the southern hemisphere",
+					  "Its official language is Spanish"
+					},
+					{ 
+					  "jazz",
+					  "A music genre",
+					  "Became very popular after the 20s",
+					  "American"
+					},
+					{ 
+					  "dumbledore",
+					  "A fictional character",
+					  "First appeared in literature, but later in film and games",
+					  "From the Harry Potter universe"
+					}
+				 };
+	
+	if ((datafile = fopen(filename, "wb")) != NULL) {
+		
+		for (i = 0; i < NUM_WORDS_DEFAULT_FILE; i++) {
+			fwrite(&words[i], sizeof(WORD), 1, datafile);
+		}
+		
+		fclose(datafile);
+		
+		return 1;
+	
+	} else {
+		return 0;
+	}
+	
+}
+
+int inspect_file(char * filename) { 
+	FILE * datafile;
+	WORD w;
+	char op;
+	
+	if ((datafile = fopen(filename, "rb")) != NULL) {
+	
+		do {
+			
+			if (fread(&w, sizeof(WORD), 1, datafile) == 1) {
+				display_word(w);
+				printf("\nEnter q to quit or any other char to continue: ");
+				scanf(" %c", &op);
+			} else {
+				printf("\nEnd of the file.\n");
+			}	
+			
+		
+		} while (op != 'q' && !feof(datafile));
+		
+		fclose(datafile);
+		
+		return 1;
+	} else {
+		return 0;
+	}
+	
+}
+
